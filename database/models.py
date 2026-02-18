f1233dad0423b630303d51d7b3e029c9560eb295
Abhishek Rawat <162818246+Abhishek8979420909@users.noreply.github.com>
2026-02-18 12:16:36 +0530

    Refactor Token model in models.py

--- database/models.py
+++ database/models.py
@@ -8,7 +8,7 @@ class Token(Base):
     __tablename__ = "tokens"
 
     id = Column(Integer, primary_key=True, index=True)
-    access_token = Column(String, nullable=False, unique=True)
+    access_token = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     expired_at = Column(DateTime, nullable=False)
 
@@ -17,4 +17,5 @@ class Token(Base):
 
 
     def __repr__(self):
+
         return f"<Token(id={self.id}, created_at={self.created_at})>"
